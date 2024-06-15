e882afe3a6ed25b96f18404db6fee08acfb79418
mehdi norouzi <mehdinorouzi.dev@gmail.com>
2024-06-15 14:29:39 +0330

    change

--- lib/features/main/data/data_source/main_Api_provider.dart
+++ lib/features/main/data/data_source/main_Api_provider.dart
@@ -1,14 +1,17 @@
 import 'package:dio/dio.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 class MainApiProvider{
   final Dio _dio = Dio();
-
   Future<dynamic> homeData(Map map) async {
+    final SharedPreferences prefs = await SharedPreferences.getInstance();
+    final String? token = prefs.getString("session");
     try {
       var response = await _dio.post('https://interview.esavis.ir/api',
           options: Options(
             headers: {
               'Content-Type': 'application/json',
+              "Authorization":"Bearer $token",
               'omax-apikey': "cc751c6d-42cd-468f-84a3-59f85dc6b944"
             },
           ),
